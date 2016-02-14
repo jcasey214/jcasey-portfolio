5a81864e6de410a079ac7427bb23db77ccb8cbc7
Jonathan Casey <jon_casey@me.com>
2016-02-14 13:08:04 -0700

    add functionality and placeholders to about and projects views

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,27 @@
+var projectList = [{
+  name: 'myFlicks',
+  thumb: 'assets/myFlicks.png',
+  technologies: 'Javascript, Bootstrap, TMDB API, Netlix Availability API, Local Storage',
+  description: 'myFlicks is a movie search site that gives the user a simple clean search feature and indicates the availability of search results on Netflix streaming. Users can also save movies to their \'list\' to keep track of movies they want to see. Upon returning to the site, the user\'s list will be updated if the Netflix availability of any of their tracked films has changed. A walkthrough can be viewed at ',
+  github: 'http://www.github.com/jcasey214/myFlicks',
+  url: 'http://www.http://casey.g15.xyz/myFlicks/build/'
+},{
+  name: 'Griffinry',
+  thumb: 'assets/griffinry.png',
+  technologies: 'Node, Express, PostreSQL, Local Authentication and Google OAuth with Passport, Knexjs, Momentjs',
+  description: 'Griffinry is a browser based virtual pet game that allows users to adopt, interact with and care for virtual Griffins. Users can signup and login with a site-specific username/password or can choose to use google authentication. After signing up, they can adopt up to 3 griffins and choose their colors and names. Over time, pet health decreases and energy increases. To recuperate health, you can take your pet to gather food/supplies.',
+  github: 'http://www.github.com/griffinry',
+  url: 'http://www.griffinry.com'
+},
+
+{
+  name: 'Stumblr',
+  technologies: 'test',
+  description: 'test',
+  github: 'http://www.github.com/jcasey214',
+  url: 'test'}];
+
+
 app.controller('AboutController', function($scope, $rootScope) {
   $scope.greeting = "About Page";
   $rootScope.currentPage = 'about';
@@ -26,6 +50,7 @@ app.controller('ResumeController', function($scope, $rootScope){
 app.controller('ProjectsController', function($scope, $rootScope){
   $scope.greeting = "Projects";
   $rootScope.currentPage = 'projects';
+  $scope.projectList = projectList;
 });
 
 app.controller('HomeController', function($scope){
