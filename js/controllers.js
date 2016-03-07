19b507782e600693adeb885cbb9a40119e1882ec
Jonathan Casey <jon_casey@me.com>
2016-03-07 08:10:46 -0700

    add video embed links

--- js/controllers.js
+++ js/controllers.js
@@ -5,7 +5,7 @@ var projectList = [{
   description: 'Stumblr is a mobile-first social barhopping app. It utilizes the Google Maps API to find bars near your current location or any location you choose to enter. Twitter functionality is integrated and allows users to tweet when they arrive at a bar on their list and tweet the badges they earn for different benchmarks. Stumblr was built in a week and a half by myself and two classmates without any prior experience using react.',
   github: 'http://www.github.com/designateddrinkers/stumblr',
   url: 'http://www.github.com/designateddrinkers/stumblr',
-  video: 'https://youtu.be/2lOGaYJfwPQ'
+  video: '<iframe width="420" height="315" src="https://www.youtube.com/embed/2lOGaYJfwPQ" frameborder="0" allowfullscreen></iframe>'
 },
 {
   name: 'Griffinry',
@@ -14,7 +14,7 @@ var projectList = [{
   description: 'Griffinry is a browser based virtual pet game that allows users to adopt, interact with and care for virtual Griffins. Users can signup and login with a site-specific username/password or can choose to use google authentication. After signing up, they can adopt up to 3 griffins and choose their colors and names. Over time, pet health decreases and energy increases. To recuperate health, you can take your pet to gather food/supplies. Griffinry was my first full stack group app and was build using an agile workflow managed with pivotal tracker.',
   github: 'http://www.github.com/griffinry',
   url: 'http://www.griffinry.com',
-  video: 'https://youtu.be/D5DP6cTqO5k'
+  video: '<iframe width="420" height="315" src="https://www.youtube.com/embed/D5DP6cTqO5k" frameborder="0" allowfullscreen></iframe>'
 },{
   name: 'myFlicks',
   thumb: 'assets/myFlicks.png',
@@ -22,7 +22,7 @@ var projectList = [{
   description: 'myFlicks is a movie search site that gives the user a simple clean search feature and indicates the availability of search results on Netflix streaming. Users can also save movies to their \'list\' to keep track of movies they want to see. Upon returning to the site, the user\'s list will be updated if the Netflix availability of any of their tracked films has changed. This was my first front-end application built after 4 weeks of Galvanize.',
   github: 'http://www.github.com/jcasey214/myFlicks',
   url: 'http://www.http://casey.g15.xyz/myFlicks/build/',
-  video: 'https://youtu.be/x0jfv_VS5F8'
+  video: '<iframe width="420" height="315" src="https://www.youtube.com/embed/x0jfv_VS5F8" frameborder="0" allowfullscreen></iframe>'
 }];
 
 
