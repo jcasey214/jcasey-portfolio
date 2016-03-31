fee3d0367914ee86f6f93ebaaea70efebeae92f0
Jonathan Casey <jon_casey@me.com>
2016-03-31 10:14:21 -0600

    update resume

--- js/controllers.js
+++ js/controllers.js
@@ -14,7 +14,7 @@ var projectList = [{
   description: 'DrawLOL is a digital version of the classic party game Paper Telephone. Participants create a game and invite their friends to join the room. Each player starts the game by writing a sentence which is then passed to the next player. The next player draws a picture based on that sentence which is passed to the next player who then writes a sentence about the picture. This continues until every player has contributed to every sheet. The sheets are then reviewed and hilarity ensues.',
   github: 'https://github.com/jcasey214/drawlol-front',
   url: 'http://www.drawlol.com',
-  video: '<iframe width="420" height="315" src="https://www.youtube.com/embed/fjGKepYhk-w" frameborder="0" allowfullscreen></iframe>'
+  video: '<iframe width="420" height="315" src="https://www.youtube.com/embed/hFBkPksRRmI" frameborder="0" allowfullscreen></iframe>'
 },
 {
   name: 'Griffinry',
