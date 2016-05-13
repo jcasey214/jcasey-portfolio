ff6c10f1df9a95e0b047aec90a1b6da76710f51d
Jonathan Casey <jon_casey@me.com>
2016-05-13 11:21:18 -0600

    reformat app

--- js/app.js
+++ js/app.js
@@ -2,33 +2,42 @@ var app = angular.module('portfolio', ['ngRoute']);
 
 app.config(function($routeProvider, $locationProvider){
   $routeProvider
+
   .when('/', {
     templateUrl: '../partials/home.html',
     controller: 'HomeController'
+
   }).when('/about', {
     templateUrl: '../partials/about.html',
     controller: 'AboutController'
   })
+
   .when('/projects', {
     templateUrl: '../partials/projects.html',
     controller: 'ProjectsController'
   })
+
   .when('/resume', {
     templateUrl: '../partials/resume.html',
     controller: 'ResumeController'
   })
+
   .otherwise({
     redirectTo: '/'
   });
+
   $locationProvider.html5Mode(true);
+
 });
 
 app.config(function($sceDelegateProvider) {
+
   $sceDelegateProvider.resourceUrlWhitelist([
     'self',
     'https://www.youtube.com/**',
     'https://s.youtube.com/**'
   ]);
+  
 });
 // function NavBarCtrl($scope){
 //   $scope.isCollapsed = true;
